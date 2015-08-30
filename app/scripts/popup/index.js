6663d7dff5911d0c6e601f8c753ef4caece44d69
Ladislav Maxa <ladislavmaxa@outlook.com>
2015-08-30 04:35:48 +0200

    popup communication

--- app/scripts/popup/index.js
+++ app/scripts/popup/index.js
@@ -1,1 +1,36 @@
 console.log(">>Hello world from popup script<<")
+
+
+
+//Extension communication - get initial store from
+/*chrome.runtime.sendMessage({
+ action: 'getStore'
+ }, function (res) {
+ console.log('getStore', res);
+ if (res) {
+ initialStore = res
+ }
+ });
+
+
+
+//Extension communication - dispatching updates
+store.subscribe(() =>
+chrome.runtime.sendMessage({
+  action: 'updateStore',
+  store: //store.getState(),
+    sender: 'xxx' //current action
+//store.getState()
+});
+
+chrome.tabs.query({}, function (tabs) {
+  var message = {
+    action: 'updateStore',
+    store: //store.getState(),
+ sender: 'xxx' //current action
+};
+for (var i = 0; i < tabs.length; ++i) {
+  chrome.tabs.sendMessage(tabs[i].id, message);
+}
+});
+);
