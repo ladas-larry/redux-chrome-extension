c0367e0afffa73c35fb753f93706275e93c9e30d
Ladislav Maxa <ladislavmaxa@outlook.com>
2015-08-30 04:35:21 +0200

    background script communication

--- app/scripts/background/index.js
+++ app/scripts/background/index.js
@@ -1,1 +1,19 @@
-console.log(">>Hello world from background script<<")
+console.log(">>Hello world from background script<<");
+
+
+//get initial store from defaults and localstorage
+
+
+
+//Extension communication - receiving updates, sending store
+/*chrome.runtime.onMessage.addListener(
+  function (req, sender, sendResponse) {
+    if (req.action === 'updateStore') {
+      store.dispatch(action)
+    }
+    if (req.action === 'getStore') {
+      sendResponse(store.getState());
+    }
+  });
+*/
+
