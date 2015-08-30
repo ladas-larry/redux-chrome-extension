e5e4656cd3c2a5d7178aa8b0da7fd8cea24e4b06
Ladislav Maxa <ladislavmaxa@outlook.com>
2015-08-30 04:35:34 +0200

    content script communication

--- app/scripts/content/index.js
+++ app/scripts/content/index.js
@@ -1,8 +1,44 @@
 console.log(">>Hello world from content scripts<<")
 
-// There we can use Babel features like destructuring
-const exampleData = {some: "data", another: {structure: "yay"}}
 
-const {another: {structure: myName}} = exampleData
 
-console.log(`>>Printing destructured constant somethingElse = '${myName}'<<`)
+//Extension communication - get initial store
+/*chrome.runtime.sendMessage({
+  action: 'getStore'
+}, function (res) {
+  console.log('getStore', res);
+  if (res) {
+    initialStore = res
+  }
+});
+
+//Extension communication - receiving updates
+chrome.runtime.onMessage.addListener(
+  function (req, sender, sendResponse) {
+    if (req.action === 'updateStore') {
+      store.dispatch(action)
+    }
+  });*/
+
+
+//Extension communication - dispatching updates
+/*store.subscribe(() =>
+chrome.runtime.sendMessage({
+  action: 'updateStore',
+  store: //store.getState(),
+    sender: 'xxx' //current action
+//store.getState()
+});
+
+//all the other tabs
+chrome.tabs.query({}, function (tabs) {
+  var message = {
+    action: 'updateStore',
+    store: //store.getState(),
+      sender: 'xxx' //current action
+};
+for (var i = 0; i < tabs.length; ++i) {
+  chrome.tabs.sendMessage(tabs[i].id, message);
+}
+});
+);*/
