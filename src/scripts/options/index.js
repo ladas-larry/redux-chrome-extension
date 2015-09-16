81d826dbb1c5ee0e86d98b9cd329969d806c60c6
Ladislav Maxa <ladislavmaxa@outlook.com>
2015-09-16 13:16:28 +0200

    options

--- src/scripts/options/index.js
+++ src/scripts/options/index.js
@@ -1,8 +1,20 @@
 import configureStore from '../shared/store/configureStore';
 import Q from 'q';
 
-var store = {};
 
+document.getElementById('save').addEventListener('click', save_options);
+
+function save_options() {
+  var initalValue = document.getElementById('initalValue').value;
+  store.dispatch({
+    type: 'UPDATE_STATE',
+    state: {persistent: {initalValue: initalValue}}
+  });
+}
+
+
+
+var store = {};
 
 //Get initial store from Background Page
 function getInitialState() {
@@ -20,20 +32,24 @@ function getInitialState() {
   return result.promise;
 }
 
+//try to save to local storage
+
 getInitialState().then(function (initialStore) {
   store = configureStore(initialStore);
 
   store.subscribe(() => {
-      let message = {
+      //Dispatching updates to Background Page
+      chrome.runtime.sendMessage({
         action: 'updateState',
         state: store.getState()
-      };
-      //Dispatching updates to Popup Window and Background Page
-      chrome.runtime.sendMessage(message);
+      });
       //Dispatching updates to the Content Scripts
       chrome.tabs.query({}, function (tabs) {
         for (var i = 0; i < tabs.length; ++i) {
-          chrome.tabs.sendMessage(tabs[i].id, message);
+          chrome.tabs.sendMessage(tabs[i].id, {
+            action: 'updateState',
+            state: store.getState()
+          });
         }
       });
     }
